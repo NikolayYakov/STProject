63ad59f6ee9d2ce0ad125e1978bbee91c859564e
Nikolay Yakov <n.n.yakov1@gmail.com>
2022-10-23 17:16:35 +0300

    set up basic project design and some basic classes and enums to be used

---
+++ STProject/Singleton/ErrorCode.cs
@@ -0,0 +1,21 @@
+﻿namespace STProject.Singleton
+{
+    public enum ErrorCode
+    {
+        OK = 0,
+        DefaultError = -1,
+        AccessDenied = -1001,
+        InvalidParameters = -1003,
+    }
+
+    public static class ErrorCodes
+    {
+        public static Dictionary<int, string> Messages { get; set; } = new Dictionary<int, string>()
+        {
+            [(int)ErrorCode.OK] = "OK",
+            [(int)ErrorCode.DefaultError] = "ERROR: INTERNAL EXCEPTION",
+            [(int)ErrorCode.AccessDenied] = "ERROR: ACCESS DENIED",
+            [(int)ErrorCode.InvalidParameters] = "ERROR: INVALID PARAMETERS",
+        };
+    }
+}
