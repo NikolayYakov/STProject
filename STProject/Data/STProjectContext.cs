133241f945e59db9fe8e0ccb84c618a0bc3aecd1
Nikolay Yakov <n.n.yakov1@gmail.com>
2022-10-23 20:55:06 +0300

    Database contex + first model + connectionString

---
+++ STProject/Data/STProjectContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using STProject.Data.Models;
+
+namespace STProject.Data
+{
+    public class STProjectContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public STProjectContext(DbContextOptions<STProjectContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
