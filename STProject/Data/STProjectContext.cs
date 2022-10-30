923f87170353b5e2d3aeb57c538163b4dc967ab8
Nikolay Yakov <n.n.yakov1@gmail.com>
2022-10-30 21:52:04 +0200

    implementing identity

--- STProject/Data/STProjectContext.cs
+++ STProject/Data/STProjectContext.cs
@@ -1,13 +1,15 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using STProject.Data.Models;
 
 namespace STProject.Data
 {
-    public class STProjectContext : DbContext
+    public class STProjectContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public DbSet<Post> Posts { get; set; }
-
+        
         public STProjectContext(DbContextOptions<STProjectContext> options)
             :base(options)
         {
